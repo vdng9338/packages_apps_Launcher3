61fa7d2ddf7ce8bbcf7c45a1fd87791609b574e7
spezi77 <spezi7713@gmx.net>
2020-12-21 16:32:30 +0100

    Launcher3: Fix launcher preview rendering (grid size). This will catch an exception when the grid preview cannot be rendered https://del.dog/ronulefufo.txt
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
+++ src/com/android/launcher3/graphics/LauncherPreviewRenderer.java
@@ -405,10 +405,17 @@ public class LauncherPreviewRenderer {
                 PreviewContext previewContext = null;
                 if (mMigrated) {
                     previewContext = new PreviewContext(mContext, mIdp);
-                    LauncherAppState appForPreview = new LauncherAppState(
-                            previewContext, null /* iconCacheFileName */);
-                    fetcher = new WorkspaceItemsInfoFromPreviewFetcher(appForPreview);
-                    MODEL_EXECUTOR.execute(fetcher);
+                    try {
+                        LauncherAppState appForPreview = new LauncherAppState(
+                                previewContext, null /* iconCacheFileName */);
+                        fetcher = new WorkspaceItemsInfoFromPreviewFetcher(appForPreview);
+                        MODEL_EXECUTOR.execute(fetcher);
+                    } catch (IllegalStateException e) {
+                        Log.d(TAG, "Error populating grid items preview", e);
+                        fetcher = new WorkspaceItemsInfoFetcher();
+                        LauncherAppState.getInstance(mContext).getModel().enqueueModelUpdateTask(
+                                (LauncherModel.ModelUpdateTask) fetcher);
+                    }
                 } else {
                     fetcher = new WorkspaceItemsInfoFetcher();
                     LauncherAppState.getInstance(mContext).getModel().enqueueModelUpdateTask(
